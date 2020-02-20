8c27669f005906ffcd2ac6bbd623142ba61ce6e1
FreedomWriter <natalienetworks@gmail.com>
2020-02-19 23:22:59 -0600

    created migrations

---
+++ data/migrations/20200219231909_inital.js
@@ -0,0 +1,15 @@
+exports.up = async function(knex) {
+  await knex.schema.createTable("users", tbl => {
+    tbl.increments();
+    tbl
+      .string("username")
+      .notNullable()
+      .unique();
+    tbl.string("password").notNullable();
+    tbl.string("department").notNullable();
+  });
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("users");
+};
